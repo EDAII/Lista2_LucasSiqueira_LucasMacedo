09e1fa64ea941aef984a27f01363ef534cf42d93
lukassxp <lpalucas.10@gmail.com>
2019-04-04 22:54:01 -0300

    Criada main para usar e comparar os algoritimos.
    Signed-off-by: lukassxp <lpalucas.10@gmail.com>
    Co-authored-by: lucassiqz <lucassiqzro@gmail.com>

--- lista2.py
+++ lista2.py
@@ -1,10 +1,14 @@
 from time import time
 from decimal import Decimal
-from random import randint
-from random import random
+import random
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+
+TAM_LISTA = 100
+NUM_REPETICOES = 1000 
+INICIO_INTERVALO = 0
+FIM_INTERVALO = 2000
 
 def plota_grafico(averages):
     plt.ylabel('Media de Tempo')
@@ -35,11 +39,11 @@ def calc_media(results):
         averages[key] = sum(results[key]) / len(results[key])
     return averages
 
-def cria_lista_sem_repeticao(tamanho):
-    return random.sample(range(0, tamanho + 1), tamanho)
+def cria_lista_sem_repeticao():
+    return random.sample(range(0, TAM_LISTA + 1), TAM_LISTA)
 
-def cria_lista_com_repeticao(tamanho, inicio_intervalo, fim_intervalo):
-    return [random.randint(inicio_intervalo, fim_intervalo) for x in range(tamanho)]
+def cria_lista_com_repeticao():
+    return [random.randint(INICIO_INTERVALO, FIM_INTERVALO) for x in range(TAM_LISTA)]
 
 def insertion_sort(alist):
     for index in range(1,len(alist)):
@@ -52,7 +56,6 @@ def insertion_sort(alist):
             position = position-1
 
         alist[position]=currentvalue
-    return alist
 
 def bubble_sort(alist):
     for passnum in range(len(alist)-1,0,-1):
@@ -61,7 +64,6 @@ def bubble_sort(alist):
                 temp = alist[i]
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
-    return alist
 
 def selection_sort(alist):
     for fillslot in range(len(alist)-1,0,-1):
@@ -73,10 +75,64 @@ def selection_sort(alist):
        temp = alist[fillslot]
        alist[fillslot] = alist[positionOfMax]
        alist[positionOfMax] = temp
-    return alist
 
 if __name__ == '__main__':
     results = {'Selection': [], 'Insertion': [], 'Bubble': []}
     result = {'Selection': 0, 'Insertion': 0, 'Bubble': 0}
     wins = {'Selection': 0, 'Insertion': 0, 'Bubble': 0}
+    verifica_empate = set()
+    qtd_empates = 0
+
+    for aux in range(NUM_REPETICOES):
+        lista = cria_lista_sem_repeticao()
+        lista_com_repeticao = cria_lista_com_repeticao()
+
+        for key in result.keys():
+            if key == 'Selection':
+                inicio = time()
+                # selection_sort(lista.copy())
+                selection_sort(lista_com_repeticao.copy())
+            elif key == 'Insertion':
+                inicio = time()
+                # insertion_sort(lista.copy())
+                insertion_sort(lista_com_repeticao.copy())
+            else: 
+                inicio = time()
+                # bubble_sort(lista.copy())
+                bubble_sort(lista_com_repeticao.copy())
+    
+            fim = time()
+            tempo = Decimal(fim - inicio)
+
+            result[key] = tempo
+            results[key].append(tempo)
+
+            verifica_empate.add(tempo)
+
+        if len(verifica_empate) != 3:
+            qtd_empates += 1
+
+        verifica_empate = set()
+         
+        lis = list(result.items())
+        winner = min(lis,key=lambda item:item[1])
+
+        wins[winner[0]] += 1
+
+
+
+    print('Quantidade de empates: ', qtd_empates)
+
+    averages = calc_media(results)
+
+    print('\n\t \t \tResultado em Wins:')
+    for key in wins.keys():
+        print(key + ': ' + str(wins[key]))
+
+    print('\n\t \t \tMedia de Tempo:')
+    for key in averages.keys():
+        print(key + ': ' + str(averages[key]))
+    
+    plota_grafico_pizza(wins)
+    plota_grafico(averages)
     
